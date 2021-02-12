f58d56bdb8940e618d13ad03c7e396ecf12ba73e
snebotcifpfbmoll <snebot@cifpfbmoll.eu>
2021-02-12 17:06:53 +0100

    added task 2

---
+++ app/src/main/java/com/android/example/wordlistsql/WordItem.java
@@ -0,0 +1,25 @@
+package com.android.example.wordlistsql;
+
+public class WordItem {
+    private int mId;
+    private int mWord;
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public int getmWord() {
+        return mWord;
+    }
+
+    public void setmWord(int mWord) {
+        this.mWord = mWord;
+    }
+
+    public WordItem() {
+    }
+}
